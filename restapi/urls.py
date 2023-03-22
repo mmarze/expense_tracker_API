5cd8e6f3393739041fc28bbffccc061a9564509e
mmarze <124877129+mmarze@users.noreply.github.com>
2023-03-22 15:21:29 +0100

    Refactored behaviour for generic views

--- restapi/urls.py
+++ restapi/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from restapi import views
 
 urlpatterns = [
-    path("expenses/", views.ExpenseListCreate.as_view(), name="expense-list-create")
+    path("expenses/", views.ExpenseListCreate.as_view(), name="expense-list-create"),
+    # <pk> -> dynamic value of the primary key
+    path(
+        "expenses/<pk>",
+        views.ExpenseRetrieveDelete.as_view(),
+        name="expense_retrieve_delete",
+    ),
 ]
