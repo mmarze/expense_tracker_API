828c0d7719584bd739d93899753aaf9fbc6f03d2
mmarze <124877129+mmarze@users.noreply.github.com>
2023-03-22 16:34:44 +0100

    deleted comment in restapi/urls.py

--- restapi/urls.py
+++ restapi/urls.py
@@ -3,7 +3,6 @@ from restapi import views
 
 urlpatterns = [
     path("expenses", views.ExpenseListCreate.as_view(), name="expense-list-create"),
-    # <pk> -> dynamic value of the primary key
     path(
         "expenses/<pk>",
         views.ExpenseRetrieveDelete.as_view(),
