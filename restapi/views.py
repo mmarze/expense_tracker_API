e000ccc2025cc20c37f23c86075c91c7ba7ec186
mmarze <124877129+mmarze@users.noreply.github.com>
2023-03-22 16:07:05 +0100

    Deleted some unused imports

--- restapi/views.py
+++ restapi/views.py
@@ -1,6 +1,3 @@
-from django.shortcuts import render
-from rest_framework.response import Response
-from django.forms.models import model_to_dict
 from restapi import models, serializers
 from rest_framework.generics import RetrieveDestroyAPIView, ListCreateAPIView
 
